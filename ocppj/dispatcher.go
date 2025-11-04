038a446c26f9ed499cf9b390c81407fdcfda3b88
Arseny <arseny@dev.dev>
2025-11-04 20:05:58 +0000

    requestChannel -> 1000, timerC -> 100

--- ocppj/dispatcher.go
+++ ocppj/dispatcher.go
@@ -403,8 +403,8 @@ func NewDefaultServerDispatcher(queueMap ServerQueueMap) *DefaultServerDispatche
 func (d *DefaultServerDispatcher) Start() {
 	d.mutex.Lock()
 	defer d.mutex.Unlock()
-	d.requestChannel = make(chan string, 20)
-	d.timerC = make(chan string, 10)
+	d.requestChannel = make(chan string, 1000)
+	d.timerC = make(chan string, 100)
 	d.stoppedC = make(chan struct{}, 1)
 	d.running = true
 	go d.messagePump()
